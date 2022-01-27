e600adf15f639507d27a7fba444b965099f65d16
Yash Aggarwal <yashbansal1979@gmail.com>
2022-01-28 00:20:32 +0530

    program completed

--- hangman.py
+++ hangman.py
@@ -11,7 +11,7 @@ def get_valid_words(words):
     return word.upper()
 
 def hangman():
-    word = get_valid_words(words)
+    word = get_valid_words(Words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
@@ -25,3 +25,31 @@ def hangman():
         print(lives_visual_dict[lives])
 
         user_letter = input('Guess a letter: ').upper()
+
+    user_letter = input('Guess a letter of your choice').upper()  # just to maintain uniformity
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+            print('')
+        else:
+            lives = lives-1 
+            print('\n Your letters,', user_letter, 'is not in the word.')
+    elif user_letter in used_letters:
+        print('\n You have already used that letter. Guess another please')
+    else:
+        print('\nThat is not a valid letter.')
+
+    # when live and len are == 0
+
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You dies, sorry. The word was', word)
+
+    else:
+        print('YAY! You gussed the word', word, '!!!!')
+
+
+
+if __name__ == '__main__':
+    hangman()
